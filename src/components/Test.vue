8ac35e6e9ca73790e482eee742588a91c0512b18
PawRys <hellbu@gmail.com>
2022-10-25 19:21:06 +0200

    Price Calculator tbc2...

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -6,32 +6,59 @@ const first_ref = ref(1)
 const second_ref = ref(1)
 const third_ref = ref(1)
 const vat = ref(false)
+const editedFieldId = ref()
+const shadowValue = ref()
 
-function whole(val) {
-	mother_ref.value = Math.round((val.target.value.trim() / 1) * 100) / 100
+function full(val) {
+	mother_ref.value = val.target.value.trim() / 1
 }
 function half(val) {
-	mother_ref.value = Math.round((val.target.value.trim() / 2) * 100) / 100
+	mother_ref.value = val.target.value.trim() / 2
 }
 function qart(val) {
-	mother_ref.value = Math.round((val.target.value.trim() / 4 / (vat.value ? 1.23 : 1)) * 100) / 100
+	if (editedFieldId.value === 'cub') shadowValue.value = val.target.value.trim()
+	mother_ref.value = val.target.value.trim() / 4 / (vat.value ? 1.23 : 1)
 }
 
-const first = computed(() => {
+const one = computed(() => {
 	return mother_ref.value * 1
 })
-const second = computed(() => {
+const two = computed(() => {
 	return mother_ref.value * 2
 })
-const third = computed(() => {
+const tre = computed(() => {
 	return mother_ref.value * 4 * (vat.value ? 1.23 : 1)
 })
+
+function test(ev) {
+	console.log(ev)
+}
 </script>
 
 <template>
 	<h2>{{ mother_ref }}</h2>
 	<label for="vat">Vat: <input type="checkbox" name="vat" id="" v-model="vat" /></label>
-	<p><input type="number" :value="first" @change="whole" />: {{ first_ref }}</p>
-	<p><input type="number" :value="second" @change="half" />: {{ second_ref }}</p>
-	<p><input type="number" :value="third" @change="qart" />: {{ third_ref }}</p>
+	<p><input type="number" :value="one.toFixed(2)" @input="full" /></p>
+	<p><input type="number" :value="two.toFixed(2)" @input="half" /></p>
+	<p>
+		<input
+			v-if="editedFieldId !== 'cub'"
+			type="number"
+			:value="tre.toFixed(2)"
+			@focus="
+				() => {
+					editedFieldId = 'cub'
+					shadowValue = tre
+				}
+			" />
+		<input
+			v-else
+			type="number"
+			:value="shadowValue"
+			:placeholder="tre.toFixed(2)"
+			@input="qart"
+			@blur="editedFieldId = ''"
+			@keypress.enter="$event.target.select()"
+			@vnode-mounted="({ el }) => el.focus()" />
+	</p>
 </template>
