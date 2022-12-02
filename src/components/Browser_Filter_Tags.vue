95aafc0a8ce79d3ae9eb2eaf30904f369881a6b9
PawRys <hellbu@gmail.com>
2022-12-02 19:31:56 +0100

    Filter Tags - properly functioning feature

--- src/components/Browser_Filter_Tags.vue
+++ src/components/Browser_Filter_Tags.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref, inject, watch, watchEffect } from 'vue';
-const tagCloud_ref = ref({
+import { ref, inject, watch, watchEffect, unref } from 'vue';
+const filteredProducts_Tags = ref({
 	tags: [],
 	thick: [],
 	sizeA: [],
@@ -8,6 +8,15 @@ const tagCloud_ref = ref({
 	grades: [],
 	words: [],
 });
+const checkedInputs = ref({
+	tags: [],
+	thick: [],
+	sizeA: [],
+	sizeB: [],
+	grades: [],
+	words: [],
+});
+
 const userFilter = inject('userFilter');
 const data = inject('filteredData_global');
 const collator = (a, b) => {
@@ -28,7 +37,7 @@ watch([userFilter, data], () => {
 		const codename = `${row.code} ${row.name}`;
 
 		if (row.tags) {
-			tags.add(...row.tags.split(' '));
+			row.tags.split(' ').map(s => tags.add(s));
 		}
 
 		if (row.size) {
@@ -40,7 +49,7 @@ watch([userFilter, data], () => {
 
 		const grade = getProductGrade(codename);
 		if (grade) {
-			grades.add(...grade);
+			grade.map(s => grades.add(s));
 		}
 
 		const chunks = codename.split(/[ \/]/gi);
@@ -50,16 +59,33 @@ watch([userFilter, data], () => {
 			words.add(c.toLowerCase().replace(/\.$/gi, ''));
 		}
 	}
-	Object.assign(tagCloud_ref.value, {
+
+	filteredProducts_Tags.value = {
 		tags: Array.from(tags).sort(collator),
 		thick: Array.from(thick).sort(collator),
 		sizeA: Array.from(sizeA).sort(collator),
 		sizeB: Array.from(sizeB).sort(collator),
 		grades: Array.from(grades).sort(collator),
 		words: Array.from(words).sort(collator),
-	});
+	};
+});
 
-	// console.log(tagCloud_ref.value);
+watchEffect(() => {
+	// watch(checkedInputs, () => {
+	console.log(`watch checkedInputs`);
+	const inputs = unref(checkedInputs);
+	console.log(inputs);
+	const eq = inputs.grades.length ? '=' : '';
+	const x = inputs.thick.length || inputs.sizeA.length || inputs.sizeB.length ? 'x' : '';
+	let tags = inputs.tags.join('|');
+	let thick = inputs.thick.join('|');
+	let sizeA = inputs.sizeA.join('|');
+	let sizeB = inputs.sizeB.join('|');
+	let grades = inputs.grades.join('|');
+	let words = inputs.words.join('|');
+
+	userFilter.value =
+		`${tags} ${thick}${x}${sizeA}${x}${sizeB} ${eq}${grades} ${words}`.trim();
 });
 
 function getProductGrade(input) {
@@ -67,17 +93,9 @@ function getProductGrade(input) {
 	const pattern = `\\b(${part}){1}(\/(${part})){0,1}(?!\\.)\\b`;
 	const grade = input.toUpperCase().match(new RegExp(pattern, 'gi'));
 	return grade;
-	// return grade ? grade : ['??/??'];
 }
-const checkedIputs = ref({
-	tags: [],
-	thick: [],
-	sizeA: [],
-	sizeB: [],
-	grades: [],
-	words: [],
-});
-function serialize(formData) {
+
+function getAllCheckedBoxes() {
 	const form = document.querySelector('#tag-selector');
 	const data = new FormData(form);
 	let tags = data.getAll('tags');
@@ -86,10 +104,8 @@ function serialize(formData) {
 	let sizeB = data.getAll('sizeB');
 	let grades = data.getAll('grades');
 	let words = data.getAll('words');
-	const eq = grades.length ? '=' : '';
-	const x = thick.length || sizeA.length || sizeB.length ? 'x' : '';
 
-	checkedIputs.value = {
+	checkedInputs.value = {
 		tags: tags,
 		thick: thick,
 		sizeA: sizeA,
@@ -97,40 +113,45 @@ function serialize(formData) {
 		grades: grades,
 		words: words,
 	};
+	// console.log(unref(checkedInputs));
+}
 
-	tags = tags.join('|');
-	thick = thick.join('|');
-	sizeA = sizeA.join('|');
-	sizeB = sizeB.join('|');
-	grades = grades.join('|');
-	words = words.join('|');
-
-	const result =
-		`${tags} ${thick}${x}${sizeA}${x}${sizeB} ${eq}${grades} ${words}`.trim();
-	userFilter.value = result;
-	console.log(checkedIputs.value);
+function clearCheckboxesInGroup(groupName) {
+	console.log(checkedInputs.value);
+	const inputs = unref(checkedInputs);
+	inputs[groupName] = [];
+	checkedInputs.value = inputs;
+	console.log(checkedInputs.value);
 }
 
 function isChecked(colId, tag) {
-	return checkedIputs.value[colId].findIndex((e) => e === tag) >= 0
-		? true
-		: false;
+	const inputs = unref(checkedInputs);
+	const test = inputs[colId].findIndex(e => e === tag) < 0 ? false : true;
+	return test;
+}
+
+function vnodelog(x) {
+	console.log(x);
 }
 </script>
 
 <template>
-	<button @click="serialize">OK</button>
-	<form id="tag-selector">
+	<button class="button accent" @click="getAllCheckedBoxes">OK</button>
+	<form id="tag-selector" action="javascript:void(0);">
 		<fieldset
-			v-for="(columnTags, colId) in tagCloud_ref"
+			v-for="(columnTags, colId) in filteredProducts_Tags"
 			:key="colId"
 			:class="['search-tags', colId]">
 			<h3>
 				{{ colId }}
 			</h3>
+			<button class="button accent" @click="getAllCheckedBoxes">Dodaj filtr</button>
+			<button class="button" @click="clearCheckboxesInGroup(colId)">
+				<span>Usuń</span><i class="bi bi-trash3"></i>
+			</button>
 			<label
 				v-for="(tag, tagIndex) in columnTags"
-				:key="`${colId}-${tagIndex}`"
+				:key="`${colId}-${tag}`"
 				:for="`${colId}-${tagIndex}`">
 				<input
 					type="checkbox"
@@ -150,5 +171,9 @@ form {
 }
 label {
 	display: block;
+	user-select: none;
+}
+.button > i {
+	font-size: 1em;
 }
 </style>
