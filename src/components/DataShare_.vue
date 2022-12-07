65a2444c2065a2c116ca9f3f8766cc7ae0f7d538
PawRys-Smetek <hellbu@gmail.com>
2022-12-07 10:40:11 +0100

    pre deploy

--- src/components/DataShare_.vue
+++ src/components/DataShare_.vue
@@ -1,19 +1,20 @@
 <script setup>
-import { ref, reactive, computed, watch, provide, inject } from 'vue'
-import { db as idb } from '../assets/dexiedb.js'
+import { ref, reactive, computed, watch, provide, inject } from 'vue';
+import { db as idb } from '../assets/dexiedb.js';
 
-const shareCode = ref()
-const password = ref()
-const message = ref()
+const shareCode = ref();
+const password = ref();
+const message = ref();
 
 async function generateCode() {
-	shareCode.value = Math.random().toString().substring(2, 6)
+	message.value = 'Wysyłanie danych...';
+	shareCode.value = Math.random().toString().substring(2, 6);
 	const URLparams = {
 		action: 'provide',
 		pin: shareCode.value,
 		password: password.value,
 		data: JSON.stringify(await idb.products.toArray()),
-	}
+	};
 
 	const fetchSettings = {
 		method: 'POST',
@@ -21,16 +22,17 @@ async function generateCode() {
 			'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
 		},
 		body: new URLSearchParams(URLparams).toString(),
-	}
+	};
 
 	const fetchResult = await fetch(
 		// 'https://bossman.hekko24.pl/apps/assets/website/app_stocks/stocks.3.php',
-		'http://localhost:3000/api/index.php',
+		'https://bossman.hekko24.pl/stock_browser_server/index.php',
+		// 'http://localhost:3000/stock_browser_server/index.php',
 		fetchSettings
-	).catch(reason => console.error(reason))
+	).catch(reason => console.error(reason));
 
 	// console.log(await msg.json())
-	message.value = (await fetchResult.json()).message
+	message.value = (await fetchResult.json()).message;
 }
 </script>
 
@@ -42,7 +44,7 @@ async function generateCode() {
 			type="password"
 			placeholder="Sekretne hasło"
 			v-model="password" />
-		<button class="button small" @click="generateCode">Generuj kod</button>
+		<button class="button small accent" @click="generateCode">Generuj kod</button>
 	</p>
 	<p id="share-code">{{ shareCode }}</p>
 	<p>{{ message }}</p>
--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/StockBrowser/',
 	plugins: [vue()],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
 	},
-	// server: {
-	// 	proxy: {
-	// 		// with options
-	// 		'/api': {
-	// 			target: 'http://localhost:3000/',
-	// 			changeOrigin: true,
-	// 			secure: false,
-	// 			ws: true,
-	// 			// rewrite: path => path.replace(/^\/api/, ''),
-	// 		},
-	// 	},
-	// },
-})
+});
