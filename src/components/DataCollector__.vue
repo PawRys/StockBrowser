287d73396e88f3ab76a4e31b58899814259b2af1
PawRys <hellbu@gmail.com>
2023-01-26 18:29:45 +0100

    WIP

--- src/components/DataCollector__.vue
+++ src/components/DataCollector__.vue
@@ -136,7 +136,7 @@ async function generateTimestamp(dataType) {
 		</p>
 	</section>
 
-	<example-data />
+	<!-- <example-data /> -->
 
 	<section class="data-collector__manual">
 		<h3>Instrukcja wprowadzania danych magazynowych z Symfonii</h3>
