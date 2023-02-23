3d09676f4abdcbf11226d06c52c65928a57c3260
PawRys-Smetek <hellbu@gmail.com>
2023-02-23 15:45:07 +0100

    fixed misspell in Browser__DataMode.vue

--- src/components/Browser__DataMode.vue
+++ src/components/Browser__DataMode.vue
@@ -4,18 +4,14 @@ import { inject } from 'vue';
 const dataMode = inject('dataMode');
 const items = {
 	dataMode__trade: { label: 'Handel', icon: 'bi bi-coin' },
-	dataMode__inventory: { label: 'Inwenaryzacja', icon: 'bi bi-calculator-fill' },
+	dataMode__inventory: { label: 'Inwentaryzacja', icon: 'bi bi-calculator-fill' },
 };
 </script>
 
 <template>
 	<section class="dataMode">
 		<h4>Tryb przeglądania</h4>
-		<label
-			class="button inline"
-			v-for="({ label, icon }, mode) in items"
-			:key="mode"
-			:for="mode">
+		<label class="button inline" v-for="({ label, icon }, mode) in items" :key="mode" :for="mode">
 			<i :class="icon"></i>
 			<span>{{ label }}: </span>
 			<input type="radio" name="dataMode" :id="mode" :value="mode" v-model="dataMode" />
